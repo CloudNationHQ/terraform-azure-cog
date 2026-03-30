736e012bb66e0d5adde8eb8b943f240e575f89a0
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2026-03-30 15:28:59 +0200

    feat: add support for cognitive account projects (#36)

---
+++ examples/projects/main.tf
@@ -0,0 +1,47 @@
+module "naming" {
+  source  = "cloudnationhq/naming/azure"
+  version = "~> 0.24"
+
+  suffix = ["demo", "dev"]
+}
+
+module "rg" {
+  source  = "cloudnationhq/rg/azure"
+  version = "~> 2.0"
+
+  groups = {
+    demo = {
+      name     = module.naming.resource_group.name_unique
+      location = "swedencentral"
+    }
+  }
+}
+
+module "cognitiveservices" {
+  source  = "cloudnationhq/cog/azure"
+  version = "~> 2.0"
+
+  naming = local.naming
+
+  account = {
+    name                       = module.naming.cognitive_account.name_unique
+    resource_group_name        = module.rg.groups.demo.name
+    location                   = module.rg.groups.demo.location
+    sku_name                   = "S0"
+    kind                       = "AIServices"
+    custom_subdomain_name      = module.naming.cognitive_account.name_unique
+    project_management_enabled = true
+
+    identity = {
+      type = "SystemAssigned"
+    }
+
+    projects = {
+      example = {
+        identity = {
+          type = "SystemAssigned"
+        }
+      }
+    }
+  }
+}
